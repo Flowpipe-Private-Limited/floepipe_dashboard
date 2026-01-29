7c3258544244f3ae57bd9f9543070a1395df8892
Vishnupatel18 <vishnupatel@ntar.com>
2026-01-29 09:53:01 +0530

    fix error after the merge

--- src/Images/Images.js
+++ src/Images/Images.js
@@ -2,43 +2,68 @@ import FlowLoginImage from '../assets/images/FlowLoginImage.webp';
 import LoginLogoImg from '../assets/images/LoginLogoImg.webp';
 import loadernew from '../assets/images/lodarnew.mp4';
 import loadernew2 from '../assets/images/lodarnew2.mp4';
+import Apikey from '../assets/images/Apikey.webp';
+import Integrationguide from '../assets/images/Integrationguide.webp';
+import startfree from '../assets/images/startfree.webp';
+import viewanalytics from '../assets/images/viewanalytics.webp';
+import graphicon from '../assets/images/graphicon.webp';
+import bellicon from '../assets/images/bellicon.webp';
+import FlowpipeLogo from '../assets/images/FlowpipeLogo.png';
+import statement from '../assets/images/statement.webp';
+import dashboardright from '../assets/images/dashboardright.webp';
+import playstore from '../assets/images/playstore.webp';
+import Appstore from '../assets/images/Appstore.webp';
+import reportsempty from '../assets/images/reportsempty.webp';
+import cardinsidelogo from '../assets/images/cardinsidelogo.webp';
+import msgicon from '../assets/images/msgicon.png';
+import Dashboard from '../assets/images/Dashboard.png';
+import Myaccount from '../assets/images/Myaccount.png';
+import Templates from '../assets/images/Templates.png';
+import product from '../assets/images/product.png';
+import reports from '../assets/images/reports.png';
+import Billingplan from '../assets/images/Billingplan.png';
+import bhim from '../assets/images/bhim.webp';
+import API from '../assets/images/API.png';
+import Apiusage from '../assets/images/Apiusage.png';
+import whitelist from '../assets/images/whitelist.webp';
+import Recharges from '../assets/images/Recharges.webp';
+import payouts from '../assets/images/payouts.webp';
+import kyc from '../assets/images/kyc.webp';
+import BBPS from '../assets/images/BBPS.webp';
 
 const images = {
-  FlowLoginImage: require("../assets/images/FlowLoginImage.webp"),
-  LoginLogoImg: require("../assets/images/LoginLogoImg.webp"),
-  loadernew: require("../assets/images/lodarnew.mp4"),
-  loadernew2: require("../assets/images/lodarnew2.mp4"),
-  Apikey: require("../assets/images/Apikey.webp"),
-  Integrationguide: require("../assets/images/Integrationguide.webp"),
-  startfree: require("../assets/images/startfree.webp"),
-  viewanalytics: require("../assets/images/viewanalytics.webp"),
-  graphicon: require("../assets/images/graphicon.webp"),
-  bellicon: require("../assets/images/bellicon.webp"),
-  FlowpipeLogo: require("../assets/images/FlowpipeLogo.png"),
-  statement: require("../assets/images/statement.webp"),
-  dashboardright: require("../assets/images/dashboardright.webp"),
-  playstore: require("../assets/images/playstore.webp"),
-  Appstore: require("../assets/images/Appstore.webp"),
-  reportsempty: require("../assets/images/reportsempty.webp"),
-  cardinsidelogo: require("../assets/images/cardinsidelogo.webp"),
-  msgicon: require("../assets/images/msgicon.png"),
-  Dashboard: require("../assets/images/Dashboard.png"),
-  Myaccount: require("../assets/images/Myaccount.png"),
-  Templates: require("../assets/images/Templates.png"),
-  product: require("../assets/images/product.png"),
-  reports: require("../assets/images/reports.png"),
-  Billingplan: require("../assets/images/Billingplan.png"),
-  bhim: require("../assets/images/bhim.webp"),
-  API: require("../assets/images/API.png"),
-  Apiusage: require("../assets/images/Apiusage.png"),
-  whitelist: require("../assets/images/whitelist.webp"),
-  Recharges: require("../assets/images/Recharges.webp"),
-  payouts: require("../assets/images/payouts.webp"),
-  kyc: require("../assets/images/kyc.webp"),
-  BBPS: require("../assets/images/BBPS.webp"),
-  // FlowLoginImage,
-  // LoginLogoImg,
-  // loadernew,
-  // loadernew2,
+  FlowLoginImage,
+  LoginLogoImg,
+  loadernew,
+  loadernew2,
+  Apikey,
+  Integrationguide,
+  startfree,
+  viewanalytics,
+  graphicon,
+  bellicon,
+  FlowpipeLogo,
+  statement,
+  dashboardright,
+  playstore,
+  Appstore,
+  reportsempty,
+  cardinsidelogo,
+  msgicon,
+  Dashboard,
+  Myaccount,
+  Templates,
+  product,
+  reports,
+  Billingplan,
+  bhim,
+  API,
+  Apiusage,
+  whitelist,
+  Recharges,
+  payouts,
+  kyc,
+  BBPS,
 };
+
 export default images;
